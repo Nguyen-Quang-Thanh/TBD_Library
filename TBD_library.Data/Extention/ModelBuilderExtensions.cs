2e0f77dc67a4e5aa409c7826e408491fdd469e87
Nguyen Quang Thanh <quangthanhn485@gmail.com>
2022-05-31 01:47:44 +0700

    create Extensions Folder

---
+++ TBD_library.Data/Extention/ModelBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBD_library.Data.Entities;
+using TBD_library.Data.Enums;
+
+namespace TBD_library.Data.Extention
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+                new Post() { Conten = "dữ liệu mẫu", Summary = "mẫu ban đầu" }
+            );
+        }
+    }
+}
