0590c4c33744aea1105122ec0e106ec285230615
Nguyen Quang Thanh <quangthanhn485@gmail.com>
2022-05-30 12:31:59 +0700

    create connectionString , appsettings.json

--- TBD_library.Data/Configurations/BookConfiguration.cs
+++ TBD_library.Data/Configurations/BookConfiguration.cs
@@ -16,18 +16,19 @@ namespace TBD_library.Data.Configurations
         {
             builder.ToTable("Books");
             builder.HasKey(x => x.Id);
-            builder.Property( x => x.Name ).IsRequired().IsUnicode();
-            builder.Property( x => x.Description).IsRequired().IsUnicode();
-            builder.Property( x => x.Summary).IsRequired().IsUnicode();
-            builder.Property( x => x.Author).IsRequired(false).IsUnicode();
-            builder.Property( x => x.Publisher).IsRequired(false).IsUnicode();
-            builder.Property( x => x.Img).IsRequired(false);
-            builder.Property( x => x.Created_at).IsRequired(false).HasDefaultValue(DateTime.Now);
-            builder.Property( x => x.Status).IsRequired(false).HasDefaultValue(eBookStatus.NotBorrwed);
+            builder.Property(x => x.Name).IsRequired().IsUnicode();
+            builder.Property(x => x.Description).IsRequired().IsUnicode();
+            builder.Property(x => x.Summary).IsRequired().IsUnicode();
+            builder.Property(x => x.Author).IsRequired(false).IsUnicode();
+            builder.Property(x => x.Publisher).IsRequired(false).IsUnicode();
+            builder.Property(x => x.Img).IsRequired(false);
+            builder.Property(x => x.Created_at).IsRequired(false).HasDefaultValue(DateTime.Now);
+            builder.Property(x => x.Status).IsRequired(false).HasDefaultValue(eBookStatus.NotBorrwed);
             builder.Property(x => x.BorrowCount).IsRequired().HasDefaultValue(0);
             builder.Property(x => x.Slug).IsRequired(false);
 
             builder.HasOne(x => x.User).WithMany(x => x.Books).HasForeignKey(x => x.User_id);
             builder.HasOne(x => x.BorrowBook).WithMany(x => x.Books).HasForeignKey(x => x.BorrowBook_id);
+        }
     }
 }
---
+++ TBD_library.Data/EF/TBD_libraryDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TBD_library.Data.EF
+{
+    internal class TBD_libraryDbContextFactory : IDesignTimeDbContextFactory<TBD_libraryDBContext>
+    {
+        public TBD_libraryDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("TBD_connectiton");
+
+            var optionsBuilder = new DbContextOptionsBuilder<TBD_libraryDBContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new TBD_libraryDBContext(optionsBuilder.Options);
+        }
+    }
+}
